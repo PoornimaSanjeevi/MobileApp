132caded6809801027dc24ec963344f2947698fc
PoornimaSanjeevi <Poornima@Poornimas-MacBook-Pro.local>
2015-10-22 23:27:24 -0700

    Getting list of installed apps

--- MobSec/app/src/main/java/com/apps/finalproj/mobsec/SignUPActivity.java
+++ MobSec/app/src/main/java/com/apps/finalproj/mobsec/SignUPActivity.java
@@ -5,18 +5,31 @@ package com.apps.finalproj.mobsec;
  */
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignUPActivity extends Activity
 {
-    EditText editTextUserName,editTextPassword,editTextConfirmPassword;
+    EditText editTextPassword,editTextConfirmPassword,editTextEmergencyContact,editTextLocation;
+    TextView editTextUserName,email;
     Button btnCreateAccount;
+    private static final String TAG = "SignUPActivity";
 
     LoginDataBaseAdapter loginDataBaseAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -28,7 +41,10 @@ public class SignUPActivity extends Activity
         loginDataBaseAdapter=loginDataBaseAdapter.open();
 
         // Get Refferences of Views
-        editTextUserName=(EditText)findViewById(R.id.editTextUserName);
+        editTextUserName=(TextView)findViewById(R.id.editTextUserName);
+        email=(TextView)findViewById(R.id.userEmail);
+        editTextEmergencyContact = (EditText) findViewById(R.id.editTextEmergencyContact);
+        editTextLocation=(EditText)findViewById(R.id.Location);
         editTextPassword=(EditText)findViewById(R.id.editTextPassword);
         editTextConfirmPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
 
@@ -39,6 +55,9 @@ public class SignUPActivity extends Activity
                 // TODO Auto-generated method stub
 
                 String userName=editTextUserName.getText().toString();
+                String emailid=email.getText().toString();
+                String emergenCon=editTextEmergencyContact.getText().toString();
+                String location=editTextLocation.getText().toString();
                 String password=editTextPassword.getText().toString();
                 String confirmPassword=editTextConfirmPassword.getText().toString();
 
@@ -65,6 +84,46 @@ public class SignUPActivity extends Activity
 
             }
         });
+            getPackages();
+    }
+
+    class PInfo {
+        private String appname = "";
+        private String pname = "";
+        private String versionName = "";
+        private int versionCode = 0;
+        private Drawable icon;
+        private void prettyPrint() {
+            Log.v(TAG,appname + "\t" + pname + "\t" + versionName + "\t" + versionCode);
+        }
+    }
+
+    private ArrayList<PInfo> getPackages() {
+        ArrayList<PInfo> apps = getInstalledApps(false); /* false = no system packages */
+        final int max = apps.size();
+        for (int i=0; i<max; i++) {
+            apps.get(i).prettyPrint();
+        }
+        return apps;
+    }
+
+    private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
+        ArrayList<PInfo> res = new ArrayList<PInfo>();
+        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        for(int i=0;i<packs.size();i++) {
+            PackageInfo p = packs.get(i);
+            if (  (p.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0){
+                continue ;
+            }
+            PInfo newInfo = new PInfo();
+            newInfo.appname = p.applicationInfo.loadLabel(getPackageManager()).toString();
+            newInfo.pname = p.packageName;
+            newInfo.versionName = p.versionName;
+            newInfo.versionCode = p.versionCode;
+            newInfo.icon = p.applicationInfo.loadIcon(getPackageManager());
+            res.add(newInfo);
+        }
+        return res;
     }
     @Override
     protected void onDestroy() {
